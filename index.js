0b51bbda88890b0882570ae357e9bd1ee494500e
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-26 03:22:07 +0600

    ratings

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000
 // clothsStore POKPCxdGyVzOipiY
 
 
+app.use(cors())
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfwpldl.mongodb.net/?retryWrites=true&w=majority`;
@@ -18,6 +20,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const allProductsCollection = client.db('clothsProducts').collection('products')
+        const ratingCollection =  client.db('clothsProducts').collection('ratings')
+        const contactCollection =  client.db('clothsProducts').collection('contact')
 
         app.get('/products', async(req, res) =>{
             const query = {}
@@ -29,6 +33,22 @@ async function run(){
             const result = await allProductsCollection.find(query).toArray()
             res.send(result)
         })
+        app.get('/ratings', async(req, res) =>{
+            const query = {}
+            const result = await ratingCollection.find(query).limit(6).toArray()
+            res.send(result)
+        })
+        app.post('/ratings', async(req, res) =>{
+            const rating = req.body
+            const result = await ratingCollection.insertOne(rating)
+            res.send(result)
+        })
+        app.post('/contact', async(req, res) =>{
+            const rating = req.body
+            const result = await contactCollection.insertOne(rating)
+            res.send(result)
+        })
+        
 
     }
     finally{
@@ -39,8 +59,6 @@ async function run(){
 run().catch(console.log())
 
 
-app.use(cors())
-app.use(express.json())
 
 app.get('/',( req, res) => {
     res.send('api is running')

591d56275aa6b36845f8540947a9a564df66e5a7
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-25 20:03:40 +0600

    products

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 // clothsProducts products
+
+// clothsStore POKPCxdGyVzOipiY
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfwpldl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const allProductsCollection = client.db('clothsProducts').collection('products')
+
+        app.get('/products', async(req, res) =>{
+            const query = {}
+            const result = await allProductsCollection.find(query).limit(6).toArray()
+            res.send(result)
+        })
+      
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log())
+
 
 app.use(cors())
 app.use(express.json())
